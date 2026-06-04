d81041d973b4b678766e774e6f41c349232bb885
Sophia <bantugansophiamae@gmail.com>
2026-06-04 13:56:30 +0800

    Hello, world again and again!!!

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ void setup() {
 Serial.begin(115200);
 Serial.println("Hello, world!");
 Serial.println("This is a test of the Serial class.");
+Serial.println("Hello, world again and again!!!")
 }
 
 void loop() {
